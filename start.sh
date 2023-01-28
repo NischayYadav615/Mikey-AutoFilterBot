fcf9d62c141a836bb8b4efa019a25a53f372b58e
Matrixswagg <123743469+Matrixswagg@users.noreply.github.com>
2023-01-28 22:09:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devil-Botz/Elsa.git /Elsa
+  https://github.com/Matrixswagg/Mikey-AutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /Mikey-AutoFilterBot
 fi
-cd /Elsa
+cd /Mikey-AutoFilterBot
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
