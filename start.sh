c63e94d56a671d3fdb8b3e86aa7c42218f55b24a
Matrixswagg <123743469+Matrixswagg@users.noreply.github.com>
2023-02-18 18:21:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Mikey-AutoFilterBot
 fi
-cd /Elsa
+cd /Mikey-AutoFilterBot
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 MIKEY....🧞‍♂️"
 python3 bot.py
