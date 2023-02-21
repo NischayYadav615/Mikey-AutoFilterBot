e84411e5795de0f365f15c7d5ed9d8f43cec9b6e
Matrixswagg <123743469+Matrixswagg@users.noreply.github.com>
2023-02-21 20:06:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://graph.org/file/48c5ac17c91f0aba5195a.jpg https://graph.org/file/7bbe25f0685bf8ffa0fdd.jpg')).split()
-NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/7d7cbf0d6c39dc5a05f5a.jpg")
+NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/1dbf6b5cf98b05c661c23.jpg")
 SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/b58f576fed14cd645d2cf.jpg")
 
 # Welcome area
